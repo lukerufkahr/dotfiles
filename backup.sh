da435d10da1a67d49f8b7b7673ca671b79c390c1
Lucas Rufkahr <lucasrufkahr@toad.vlg.garynet.org>
2025-08-05 10:02:07 -0500

    commit

--- backup.sh
+++ backup.sh
@@ -9,29 +9,15 @@ read -p "Enter message: " commit_message
 
 ### Copy files ###
 
-# Vim
-cp -r ~/.vimrc ~/Git/dotfiles/
+rsync ~/.vimrc ~/Git/dotfiles/
 
-# Sway
-cp -r ~/.config/sway/* ~/Git/dotfiles/.config/sway/
+rsync -r ~/.config/i3/ ~/Git/dotfiles/.config/
 
-# Waybar
-cp -r ~/.config/waybar/* ~/Git/dotfiles/.config/waybar/
+rsync ~/.Xresources ~/Git/dotfiles/
 
-# Foot
-cp -r ~/.config/foot/* ~/Git/dotfiles/.config/foot/
+rsync -r ~/.icons ~/Git/dotfiles
 
-# Mpd
-cp -r ~/.config/mpd/* ~/Git/dotfiles/.config/mpd/
-
-# Ncmpcpp
-cp -r ~/.config/ncmpcpp/* ~/Git/dotfiles/.config/ncmpcpp/
-
-# Qutebrowser
-cp -r ~/.config/qutebrowser/* ~/Git/dotfiles/.config/qutebrowser/
-
-# Ranger
-cp -r ~/.config/ranger/* ~/Git/dotfiles/.config/ranger/
+rsync -r ~/.config/picom/ ~/Git/dotfiles/.config/
 
 ### Git ###
 
